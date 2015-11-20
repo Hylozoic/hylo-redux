44bd530da9107cd8d8404ba22072a061d7a28c94
Lawrence Wang <l@lw.io>
2015-11-19 20:33:00 -0800

    get prefetching working on client

--- src/client/index.js
+++ src/client/index.js
@@ -4,14 +4,29 @@ import { render } from 'react-dom'
 import { configureStore } from '../store'
 import createHistory from 'history/lib/createBrowserHistory'
 import { Provider } from 'react-redux'
-import { Router } from 'react-router'
+import { match, Router } from 'react-router'
 import makeRoutes from '../routes'
 import { syncReduxAndRouter } from 'redux-simple-router'
+import { getPrefetchedData, getDeferredData } from 'react-fetcher'
 
 const store = configureStore(window.INITIAL_STATE)
 const routes = makeRoutes(store)
 const history = createHistory()
 syncReduxAndRouter(history, store)
+
+history.listen(location => {
+  match({routes, location}, (error, redirectLocation, renderProps) => {
+    const components = renderProps.routes.map(r => r.component)
+    const locals = {
+      path: renderProps.location.pathname,
+      query: renderProps.location.query,
+      params: renderProps.params,
+      dispatch: store.dispatch
+    }
+    getPrefetchedData(components, locals)
+    .then(() => getDeferredData(components, locals))
+  })
+})
 
 const component = (
   <Provider store={store}>

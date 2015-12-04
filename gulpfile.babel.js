8d27e17fd33e44bf53d5c4520d76f4aa820de94a
Lawrence Wang <l@lw.io>
2015-12-03 18:15:23 -0800

    set up "autotest" npm script
    runs all tests when a src or test file changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,8 +4,9 @@ import livereload from 'gulp-livereload'
 import config from './config'
 import browserify from './tasks/browserify'
 import less from './tasks/less'
+import { spawn } from 'child_process'
 
-gulp.task('watch-js', browserify.watch())
+gulp.task('watch-js', () => browserify.watch())
 gulp.task('bundle-dist-js', browserify.bundle)
 gulp.task('bundle-dev-css', less.dev)
 gulp.task('bundle-dist-css', less.dist)
@@ -16,6 +17,15 @@ gulp.task('serve', function () {
     exec: './node_modules/.bin/babel-node',
     ignore: ['public/**/*']
   })
+})
+
+gulp.task('test', function () {
+  let cmd = 'env LOG_LEVEL=warn npm test -- -R progress'.split(' ')
+  spawn(cmd[0], cmd.slice(1, cmd.length), {stdio: 'inherit'})
+})
+
+gulp.task('autotest', function () {
+  gulp.watch(['src/**/*', 'test/**/*'], ['test'])
 })
 
 gulp.task('watch', function () {

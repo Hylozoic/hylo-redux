2f2f5f92ea038769edd21362712514131fa05387
Robbie Carlton <robbie.carlton@gmail.com>
2015-12-05 13:48:48 -0800

    refactor RSVPControl

--- src/components/RSVPControl.js
+++ src/components/RSVPControl.js
@@ -15,64 +15,58 @@ export default class RSVPControl extends React.Component {
   }
 
   render () {
-    var props = this.props
-    let { post, currentUser } = props
+    let { post, currentUser, dispatch } = this.props
 
-    var yeses = filter(post.responders, er => er.response === 'yes')
-    var maybes = filter(post.responders, er => er.response === 'maybe')
-    var nos = filter(post.responders, er => er.response === 'no')
+    var respondersByType = [
+      {title: 'Going', response: 'yes', responders: filter(post.responders, er => er.response === 'yes')},
+      {title: 'Maybe', response: 'maybe', responders: filter(post.responders, er => er.response === 'maybe')},
+      {title: 'Can\'t Go', response: 'no', responders: filter(post.responders, er => er.response === 'no')}
+    ]
     var eventResponse = (filter(post.responders, responder => responder.id === currentUser.id)[0] || {response: ''}).response
-    var yesHeader = {header: 'Going', id: -1}
-    var maybeHeader = {header: 'Maybe', id: -2}
-    var noHeader = {header: 'Can\'t Go', id: -3}
 
-    var responderList = []
-    if (yeses.length > 0) {
-      responderList.push(yesHeader)
-      responderList = responderList.concat(yeses)
-    }
-    if (maybes.length > 0) {
-      responderList.push(maybeHeader)
-      responderList = responderList.concat(maybes)
-    }
-    if (nos.length > 0) {
-      responderList.push(noHeader)
-      responderList = responderList.concat(nos)
+    const pickResponse = choice => dispatch(changeEventResponse(post.id, choice, currentUser))
+
+    const renderRSVPButton = (responderList) => {
+      return (
+        <button type='button' onClick={() => pickResponse(responderList.response)} className={cx(`rsvp-#{responderList.response}`, 'btn', 'btn-default', {'active': eventResponse === responderList.response})}>
+          {responderList.title}
+          {responderList.responders.length > 0 && ` (${responderList.responders.length})`}
+        </button>
+      )
     }
 
-    var eventResponseClicked = function (response) {
-      return e => {
-        props.dispatch(changeEventResponse(post.id, response, currentUser))
-      }
+    const renderResponderList = responderList => {
+      if (responderList.responders.length === 0) return
+
+      return (
+        <span key={responderList.title}>
+          <li className='header'>
+            <span className='header'>{responderList.title}</span>
+          </li>
+          {responderList.responders.map(renderResponder)}
+        </span>
+      )
+    }
+
+    const renderResponder = responder => {
+      return (
+        <li key={responder.id}>
+          <span className='person'>
+            <Avatar person={responder} /> <Link className='person' to={`/u/${responder.id}`}><span>{responder.name}</span></Link>
+          </span>
+        </li>
+      )
     }
 
     return (
       <div className='rsvp-controls post-section'>
         <div className='btn-group buttons'>
-          <button type='button' onClick={eventResponseClicked('yes')} className={cx('rsvp-yes', 'btn', 'btn-default', {'active': eventResponse === 'yes'})}>
-            Going
-            {yeses.length > 0 && ` (${yeses.length})`}
-          </button>
-          <button type='button' onClick={eventResponseClicked('maybe')} className={cx('rsvp-maybe', 'btn', 'btn-default', {'active': eventResponse === 'maybe'})}>
-            Maybe
-            {maybes.length > 0 && ` (${maybes.length})`}
-          </button>
-          <button type='button' onClick={eventResponseClicked('no')} className={cx('rsvp-no', 'btn', 'btn-default', {'active': eventResponse === 'no'})}>
-            {"Can't Go"}
-            {nos.length > 0 && ` (${nos.length})`}
-          </button>
+          {respondersByType.map(renderRSVPButton)}
         </div>
 
         {post.responders.length > 0 && <div className='responses'>
-
           <Dropdown className='responses-dropdown' toggleChildren={<span>See Responses</span>}>
-            {responderList.map(personOrHeader => <li key={personOrHeader.id} className={personOrHeader.header ? 'header' : ''}>
-              {personOrHeader.header
-                ? <span className='header'>{personOrHeader.header}</span>
-                : <span className='person'>
-                <Avatar person={personOrHeader} /> <Link className='person' to={`/u/${personOrHeader.id}`}><span>{personOrHeader.name}</span></Link>
-              </span>}
-            </li>)}
+            {respondersByType.map(renderResponderList)}
           </Dropdown>
         </div>}
       </div>

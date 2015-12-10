d860e67140a6e6005b7d28198a528060321ebff8
Robbie Carlton <robbie.carlton@gmail.com>
2015-12-10 12:06:59 -0800

    add skeleton of community settings

--- src/containers/community/CommunitySettings.js
+++ src/containers/community/CommunitySettings.js
@@ -1,1 +1,16 @@
-export var CommunitySettings = ''
+import React from 'react'
+import { connect } from 'react-redux'
+const { object } = React.PropTypes
+
+@connect((state, { params }) => ({community: state.communities[params.id]}))
+export default class CommunitySettings extends React.Component {
+  static propTypes = {
+    community: object
+  }
+
+  render () {
+    let { community } = this.props
+
+    return <div>Community Settings for {community.name}</div>
+  }
+}

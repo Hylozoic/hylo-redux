471eb56981033caf1066eb43c88e9005ec0c0abb
Lawrence Wang <l@lw.io>
2015-12-09 18:50:22 -0800

    don't upload files that already exist

--- tasks/upload.js
+++ tasks/upload.js
@@ -5,10 +5,23 @@ import { promisify } from 'bluebird'
 import config from '../config'
 import { walk } from 'walk'
 
+const readFile = promisify(fs.readFile)
+
+const upload = (path, destPath, put) => {
+  return readFile(path)
+  .then(body => put({
+    Key: destPath,
+    Body: body,
+    ContentType: mime.lookup(path),
+    Bucket: config.s3.bucket,
+    ACL: 'public-read'
+  }))
+}
+
 export default function () {
   var s3 = new AWS.S3()
+  let head = promisify(s3.headObject, {context: s3})
   let put = promisify(s3.putObject, {context: s3})
-  let readFile = promisify(fs.readFile)
 
   let walker = walk('dist')
 
@@ -16,17 +29,18 @@ export default function () {
     walker.on('file', (root, stats, next) => {
       let path = `${root}/${stats.name}`
       let destPath = path.replace(/^dist/, 'misc/lively')
-      console.log(destPath)
 
-      readFile(path)
-      .then(body => put({
-        Key: destPath,
-        Body: body,
-        ContentType: mime.lookup(path),
-        Bucket: config.s3.bucket,
-        ACL: 'public-read'
-      }))
-      .then(next)
+      head({Bucket: config.s3.bucket, Key: destPath})
+      .then(() => {
+        // do nothing if file exists
+        console.log(`exists: ${destPath}`)
+        next()
+      })
+      .catch(err => {
+        if (!err.code || err.code !== 'NotFound') throw err
+        console.log(`uploading: ${destPath}`)
+        upload(path, destPath, put).then(next)
+      })
     })
 
     walker.on('errors', reject)

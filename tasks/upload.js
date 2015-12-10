f8f6024f5864b8c297b1dd529a66c726bbb50381
Lawrence Wang <l@lw.io>
2015-12-09 17:30:07 -0800

    fix uploading task

--- tasks/upload.js
+++ tasks/upload.js
@@ -2,17 +2,18 @@ const AWS = require('aws-sdk')
 const fs = require('fs')
 const Promise = require('bluebird')
 const mime = require('mime')
+import config from '../config'
 
 module.exports = function () {
   var s3 = new AWS.S3()
-  var put = Promise.promisify(s3.putObject, s3)
+  var put = Promise.promisify(s3.putObject, {context: s3})
 
   return Promise.map(fs.readdirSync('dist'), function (filename) {
     return put({
       Key: 'misc/lively/' + filename,
       Body: fs.readFileSync('dist/' + filename),
       ContentType: mime.lookup(filename),
-      Bucket: process.env.AWS_S3_BUCKET,
+      Bucket: config.s3.bucket,
       ACL: 'public-read'
     })
   })

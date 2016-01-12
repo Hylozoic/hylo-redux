892393fc4ffec9c17105d118b986ad28bd98c22e
Robbie Carlton <robbie.carlton@gmail.com>
2016-01-12 12:21:48 -0800

    update api call for project contributors

--- src/actions/fetchPeople.js
+++ src/actions/fetchPeople.js
@@ -4,16 +4,18 @@ import { FETCH_PEOPLE } from './index'
 export function fetchPeople (opts) {
   let { subject, id, limit, offset, search, cacheId } = opts
   if (!offset) offset = 0
-  let querystring = cleanAndStringify({search, limit, offset})
   let payload = {api: true}
   let cache = {id: cacheId, bucket: 'peopleByQuery', limit, offset, array: true}
+  var querystring
 
   switch (subject) {
     case 'community':
+      querystring = cleanAndStringify({search, limit, offset})
       payload.path = `/noo/community/${id}/members?${querystring}`
       break
     case 'project':
-      payload.path = `/noo/project/${id}/users-redux?${querystring}`
+      querystring = cleanAndStringify({search, limit, offset, paginate: true})
+      payload.path = `/noo/project/${id}/users?${querystring}`
       break
   }
 

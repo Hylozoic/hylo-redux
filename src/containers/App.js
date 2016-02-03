e97302a41c1e01e448975b9f6e090f5d720cdc6b
Lawrence Wang <l@lw.io>
2016-02-03 13:52:46 -0800

    make App a stateless component

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,40 +8,36 @@ const { bool, func, number, object } = React.PropTypes
 
 const lastViewed = m => -Date.parse(m.last_viewed_at || '2001-01-01')
 
-@connect(({ mainMenuOpened, people }) => ({
+const App = connect(({ mainMenuOpened, people }) => ({
   currentUser: people.current,
   mainMenuOpened
-}))
-export default class App extends React.Component {
-  static propTypes = {
-    children: object,
-    count: number,
-    currentUser: object,
-    dispatch: func,
-    mainMenuOpened: bool
-  }
+}))(({ currentUser, dispatch, mainMenuOpened, children }) => {
+  let communities = currentUser
+    ? sortBy(currentUser.memberships, lastViewed).map(m => m.community)
+    : []
 
-  render () {
-    let { currentUser, dispatch, mainMenuOpened } = this.props
-    let communities = currentUser
-      ? sortBy(currentUser.memberships, lastViewed).map(m => m.community)
-      : []
+  let unreadCount = get(currentUser, 'new_notification_count') || 0
 
-    let unreadCount = get(currentUser, 'new_notification_count') || 0
-
-    return <div>
-      <div className='row'>
-        <TopNav currentUser={currentUser}
-          toggleMenu={() => dispatch(toggleMainMenu())}
-          mainMenuOpened={mainMenuOpened}
-          logout={() => dispatch(logout())}/>
-      </div>
-      <div className='row' id='mainRow'>
-        <LeftNav communities={communities} open={mainMenuOpened} unreadCount={unreadCount}/>
-        <div id='main'>
-          {this.props.children}
-        </div>
-      </div>
+  return <div>
+    <div className='row'>
+      <TopNav currentUser={currentUser}
+        toggleMenu={() => dispatch(toggleMainMenu())}
+        mainMenuOpened={mainMenuOpened}
+        logout={() => dispatch(logout())}/>
     </div>
-  }
+    <div className='row' id='mainRow'>
+      <LeftNav communities={communities} open={mainMenuOpened} unreadCount={unreadCount}/>
+      <div id='main'>{children}</div>
+    </div>
+  </div>
+})
+
+App.propTypes = {
+  children: object,
+  count: number,
+  currentUser: object,
+  dispatch: func,
+  mainMenuOpened: bool
 }
+
+export default App

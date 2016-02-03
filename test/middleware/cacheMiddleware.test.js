c54d3862858e7d71418c60cff50dc32a372c3198
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-02 17:55:24 -0800

    fix tests

--- test/middleware/cacheMiddleware.test.js
+++ test/middleware/cacheMiddleware.test.js
@@ -27,7 +27,7 @@ describe('cacheMiddleware', () => {
   it('stops the chain if a cache hit is found', () => {
     let action = {meta: {cache: {bucket: 'foo', id: 'bar'}}}
     state.foo = {bar: 'bar'}
-    expect(middleware(next)(action)).to.be.undefined
+    expect(middleware(next)(action)).to.deep.equal(Promise.resolve(action))
     expect(next).not.to.have.been.called()
   })
 
@@ -35,7 +35,7 @@ describe('cacheMiddleware', () => {
     it('counts a cache hit if the data is past the offset', () => {
       let action = {meta: {cache: {bucket: 'foo', id: 'bar', array: true, offset: 5}}}
       state.foo = {bar: [1, 2, 3, 4, 5, 6]}
-      expect(middleware(next)(action)).to.be.undefined
+      expect(middleware(next)(action)).to.deep.equal(Promise.resolve(action))
       expect(next).not.to.have.been.called()
     })
 

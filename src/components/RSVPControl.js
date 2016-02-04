3a5dee873c92bc304c42b4ca4a6260a4d5361adb
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-03 18:31:31 -0800

    fix keys warning

--- src/components/RSVPControl.js
+++ src/components/RSVPControl.js
@@ -1,47 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router'
 import cx from 'classnames'
-const { object, func } = React.PropTypes
+const { object, func, array, string } = React.PropTypes
 import { filter } from 'lodash'
 import Avatar from './Avatar'
 import Dropdown from './Dropdown'
 
-const renderRSVPButton = (eventResponse, responderList, onPickResponse) => {
-  return (
-    <button type='button' onClick={() => onPickResponse(responderList.response)} className={cx(`rsvp-#{responderList.response}`, 'btn', 'btn-default', {'active': eventResponse === responderList.response})}>
+const RSVPButton = props => {
+  let { eventResponse, responderList, onPickResponse } = props
+  return <button type='button' onClick={() => onPickResponse(responderList.response)} className={cx(`rsvp-#{responderList.response}`, 'btn', 'btn-default', {'active': eventResponse === responderList.response})}>
       {responderList.title}
       {responderList.responders.length > 0 && ` (${responderList.responders.length})`}
     </button>
-  )
 }
 
-const renderResponder = responder => {
-  return (
-    <li key={responder.id}>
+const Responder = props => {
+  let { responder } = props
+  return <li key={responder.id}>
       <span className='person'>
         <Avatar person={responder} /> <Link className='person' to={`/u/${responder.id}`}><span>{responder.name}</span></Link>
       </span>
     </li>
-  )
 }
 
-const renderResponderList = responderList => {
-  if (responderList.responders.length === 0) return
-  return (
-    <span key={responderList.title}>
-      <li className='header'>
-        <span className='header'>{responderList.title}</span>
-      </li>
-      {responderList.responders.map(renderResponder)}
-    </span>
-  )
+const ResponderList = props => {
+  let {responderList} = props
+  if (responderList.responders.length === 0) return <span />
+  return <span>
+    <li className='header'>
+      <span className='header'>{responderList.title}</span>
+    </li>
+    {responderList.responders.map(r => <Responder responder={r} key={r.id} />)}
+  </span>
 }
 
 export default class RSVPControl extends React.Component {
   static propTypes = {
+    responders: array,
     currentUser: object,
     post: object,
-    dispatch: func
+    onPickResponse: func,
+    currentResponse: string
   }
 
   render () {
@@ -56,12 +55,12 @@ export default class RSVPControl extends React.Component {
     return (
       <div className='rsvp-controls post-section'>
         <div className='btn-group buttons'>
-          {respondersByType.map(rl => renderRSVPButton(currentResponse, rl, onPickResponse))}
+          {respondersByType.map(rl => <RSVPButton currentResponse={currentResponse} responderList={rl} onPickResponse={onPickResponse} key={rl.response}/>)}
         </div>
 
         {responders.length > 0 && <div className='responses'>
           <Dropdown className='responses-dropdown' toggleChildren={<span>See Responses</span>}>
-            {respondersByType.map(renderResponderList)}
+            {respondersByType.map(rl => <ResponderList responderList={rl} key={rl.response}/>)}
           </Dropdown>
         </div>}
       </div>

5c27d7646dc7110706c1c3d1967b799ec0c53b37
Lawrence Wang <l@lw.io>
2016-02-08 13:19:08 -0800

    add missing <li> tags to LeftNav

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -5,22 +5,24 @@ import cx from 'classnames'
 const LeftNav = ({ communities, open, unreadCount }) => {
   return <nav id='leftNav' className={cx({open})}>
     <ul>
-      <A to='/all-posts'>All posts</A>
-      <A to='/my-posts'>My posts</A>
-      <A to='/followed-posts'>Followed posts</A>
-      <A to='/projects'>Projects</A>
-      <A to='/notifications'>
-        Notifications
-        {unreadCount > 0 ? ` (${unreadCount})` : ''}
-      </A>
+      <li><A to='/all-posts'>All posts</A></li>
+      <li><A to='/my-posts'>My posts</A></li>
+      <li><A to='/followed-posts'>Followed posts</A></li>
+      <li><A to='/projects'>Projects</A></li>
+      <li>
+        <A to='/notifications'>
+          Notifications
+          {unreadCount > 0 ? ` (${unreadCount})` : ''}
+        </A>
+      </li>
       {communities.map((c, i) => <li key={i}>
         <A to={`/c/${c.slug}`}>
           <img src={c.avatar_url}/>
           {c.name}
         </A>
       </li>)}
-      <A to='/c/new'>Create a community</A>
-      <A to='/c/join'>Join a community</A>
+      <li><A to='/c/new'>Create a community</A></li>
+      <li><A to='/c/join'>Join a community</A></li>
     </ul>
   </nav>
 }

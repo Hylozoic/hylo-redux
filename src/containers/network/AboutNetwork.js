3c3567c7b10f7174bd7b40b0bfe9763cedc1696e
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-11 17:56:43 -0800

    about network

--- src/containers/network/AboutNetwork.js
+++ src/containers/network/AboutNetwork.js
@@ -1,3 +1,17 @@
 import React from 'react'
-const AboutNetwork = props => <span />
-export default AboutNetwork
+import { connect } from 'react-redux'
+import { markdown } from '../../util/text'
+const { object } = React.PropTypes
+
+@connect((state, { params }) => ({network: state.networks[params.id]}))
+export default class AboutNetworks extends React.Component {
+  static propTypes = {
+    network: object
+  }
+
+  render () {
+    let { network } = this.props
+    return <div className='markdown description'
+      dangerouslySetInnerHTML={{__html: markdown(network.description)}}/>
+  }
+}

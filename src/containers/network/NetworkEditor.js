174a3f0dac3b9d5032a7ec2916a649fadd0e309f
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-18 12:36:08 -0800

    additions to network editor

--- src/containers/network/NetworkEditor.js
+++ src/containers/network/NetworkEditor.js
@@ -5,6 +5,12 @@ import { fetchCommunities } from '../../actions/fetchCommunities'
 import { fetchWithCache, connectedListProps } from '../../util/caching'
 import CommunityCards from '../../components/CommunityCards'
 import {
+  CREATE_NETWORK,
+  UPLOAD_IMAGE,
+  navigate,
+  typeahead
+} from '../../actions'
+import {
   createNetwork,
   resetNetworkValidation,
   updateNetworkEditor,
@@ -12,7 +18,13 @@ import {
   fetchNetwork,
   updateNetwork
 } from '../../actions/network'
+import { some, omit, filter, startsWith, contains } from 'lodash'
 const { array, bool, func, number, object, string } = React.PropTypes
+
+const defaultBanner = 'https://d3ngex8q79bk55.cloudfront.net/misc/default_community_banner.jpg'
+const defaultAvatar = 'https://d3ngex8q79bk55.cloudfront.net/misc/default_community_avatar.png'
+
+const typeaheadId = 'network_communities'
 
 const subject = 'network'
 const fetch = fetchWithCache(fetchCommunities)
@@ -23,10 +35,33 @@ const query = {offset: 0, limit: 1000}
   return dispatch(fetch(subject, id, query))
 })
 @connect((state, { params: { id } }) => {
+  let { people, networks, networkValidation, pending, typeaheadMatches, networkEdits } = state
+  let validating = some(networkValidation.pending)
+  let saving = pending[CREATE_NETWORK]
+  let uploadingImage = !!pending[UPLOAD_IMAGE]
+  let currentUser = people.current
+
+  if (!id) id = 'new'
+
+  let network = networks[id]
+
+  let networkEdit = {...network, ...networkEdits[id]}
+
+  let { errors } = networkEdit
+
+  if (!errors) errors = {}
+  // errors.nameUsed = get(networkValidation, 'name.unique') === false
+  // errors.slugUsed = get(networkValidation, 'slug.unique') === false
+
+  if (!networkEdit.avatar_url) networkEdit.avatar_url = defaultAvatar
+  if (!networkEdit.banner_url) networkEdit.banner_url = defaultBanner
+
+  let clp = connectedListProps(state, {subject, id, query}, 'communities')
+
   return {
     ...connectedListProps(state, {subject, id, query}, 'communities'),
-    network: state.networks[id],
-    currentUser: state.people.current
+    id, network, networkEdit, errors, validating, saving, uploadingImage, currentUser,
+    communityChoices: typeaheadMatches[typeaheadId] || []
   }
 })
 export default class NetworkEditor extends React.Component {
@@ -51,9 +86,6 @@ export default class NetworkEditor extends React.Component {
   render () {
     let { pending, communities, currentUser, network } = this.props
     if (!currentUser) return <div>Loading...</div>
-
-    console.log('in render, network', network)
-    console.log('in render, network.name', network.name)
 
     return <div className='communities'>
       <div className='list-controls'>

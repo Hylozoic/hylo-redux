72b7e3fd28b2813bd523dc13310f0526afacee23
Lawrence Wang <l@lw.io>
2016-02-19 14:24:29 -0800

    allow clicking on tags in search to search again

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import cx from 'classnames'
 
-const Tags = ({ children, className }) =>
+const Tags = ({ children, className, onClick }) =>
   <ul className={cx('tags', className)}>
-    {children.map(tag => <div className='tag' key={tag}>{tag}</div>)}
+    {children.map(tag =>
+      <div className={cx('tag', onClick ? 'clickable' : null)}
+        key={tag} onClick={() => onClick(tag)}>
+        {tag}
+      </div>)}
   </ul>
 
 export default Tags
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { compose } from 'redux'
 import { prefetch } from 'react-fetcher'
 import { connect } from 'react-redux'
 import { connectedListProps, fetchWithCache, refetch } from '../util/caching'
@@ -12,6 +11,7 @@ import A from '../components/A'
 import Post from '../components/Post'
 import ScrollListener from '../components/ScrollListener'
 import Tags from '../components/Tags'
+const { array, bool, func, number, object } = React.PropTypes
 
 const types = [
   {name: 'Everything'},
@@ -23,52 +23,86 @@ const types = [
 const subject = 'search'
 const fetch = fetchWithCache(search)
 
-const Search = compose(
-  prefetch(({ dispatch, query }) => query.q && dispatch(fetch(subject, null, query))),
-  connect((state, { location: { query } }) => ({
-    ...connectedListProps(state, {subject, query}, 'searchResults')
-  }))
-)(({ dispatch, location, searchResults, total, pending }) => {
-  let updateSearch = debounce(opts => dispatch(refetch(opts, location)), 500)
-  let { q, type } = location.query
-  let selectedType = types.find(t => t.id === type) || types[0]
-
-  let loadMore = () => {
-    let offset = searchResults.length
-    if (!pending && offset < total) {
-      dispatch(fetch(subject, null, {q, type, offset}))
-    }
+@prefetch(({ dispatch, query }) => query.q && dispatch(fetch(subject, null, query)))
+@connect((state, { location: { query } }) => ({
+  ...connectedListProps(state, {subject, query}, 'searchResults')
+}))
+export default class Search extends React.Component {
+  static propTypes = {
+    dispatch: func,
+    location: object,
+    searchResults: array,
+    total: number,
+    pending: bool
   }
 
-  return <div id='search'>
-    <h2>Search!</h2>
-    <div className='list-controls'>
-      <input type='text' className='form-control search' defaultValue={q}
-        onChange={event => updateSearch({q: event.target.value})}/>
-      <Select className='type' choices={types} selected={selectedType}
-        onChange={t => updateSearch({type: t.id})} alignRight={true}/>
+  constructor (props) {
+    super(props)
+    this.state = {textInput: get(props, 'location.query.q')}
+  }
+
+  updateSearch = debounce(opts => {
+    let { dispatch, location } = this.props
+    return dispatch(refetch(opts, location))
+  }, 500)
+
+  render () {
+    let { dispatch, location, searchResults, total, pending } = this.props
+    if (pending && !searchResults) {
+      return <div>Loading...</div>
+    }
+
+    let { q, type } = location.query
+    let selectedType = types.find(t => t.id === type) || types[0]
+
+    let loadMore = () => {
+      let offset = searchResults.length
+      if (!pending && offset < total) {
+        dispatch(fetch(subject, null, {q, type, offset}))
+      }
+    }
+
+    let updateTextInput = text => {
+      this.setState({textInput: text})
+      this.updateSearch({q: text})
+    }
+
+    return <div id='search'>
+      <h2>Search!</h2>
+      <div className='list-controls'>
+        <input type='text' className='form-control search'
+          value={this.state.textInput}
+          onChange={event => updateTextInput(event.target.value)}/>
+        <Select className='type' choices={types} selected={selectedType}
+          onChange={t => this.updateSearch({type: t.id})} alignRight={true}/>
+      </div>
+      <Results results={searchResults}
+        dispatch={dispatch}
+        onTagClick={tag => updateTextInput(tag)}
+        loadMore={loadMore}/>
     </div>
-    <div className='results'>
-      {searchResults.map(({ type, data }) => <div key={`${type}${data.id}`}>
-        {type === 'post'
-          ? <PostResult post={data} dispatch={dispatch}/>
-          : type === 'person'
-            ? <PersonResult person={data}/>
-          : <CommentResult comment={data}/>}
-      </div>)}
-    </div>
+  }
+}
+
+const Results = ({ results, dispatch, onTagClick, loadMore }) => {
+  return <div className='results'>
+    {results.map(({ type, data }) => <div key={`${type}${data.id}`}>
+      {type === 'post'
+        ? <PostResult post={data} dispatch={dispatch}/>
+        : type === 'person'
+          ? <PersonResult person={data} onTagClick={onTagClick}/>
+        : <CommentResult comment={data}/>}
+    </div>)}
     <ScrollListener onBottom={loadMore}/>
   </div>
-})
-
-export default Search
+}
 
 const PostResult = ({ post, dispatch }) => {
   post.communities = []
   return <Post post={post} onExpand={() => dispatch(navigate(`/p/${post.id}`))}/>
 }
 
-const PersonResult = ({ person }) => {
+const PersonResult = ({ person, onTagClick }) => {
   let { bio, work, intention, skills, organizations } = person
   return <div className='person-result'>
     <div className='hello'>
@@ -82,11 +116,11 @@ const PersonResult = ({ person }) => {
       {intention && <p><strong>What I'd like to do:</strong> {intention}</p>}
       {some(skills) && <div className='tag-group skills'>
         <strong>Skills:</strong>
-        <Tags>{skills}</Tags>
+        <Tags onClick={onTagClick}>{skills}</Tags>
       </div>}
       {some(organizations) && <div className='tag-group'>
         <strong>Groups:</strong>
-        <Tags>{organizations}</Tags>
+        <Tags onClick={onTagClick}>{organizations}</Tags>
       </div>}
     </div>
   </div>

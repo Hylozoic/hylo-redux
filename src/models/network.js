9332183cf972e3d785df14a8e30b38b14c18d5b2
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-22 13:01:59 -0800

    Revert "Revert "Network Admin""

--- src/models/network.js
+++ src/models/network.js
@@ -1,14 +1,14 @@
 export const MemberRole = {DEFAULT: 0, MODERATOR: 1}
 
 export const avatarUploadSettings = network => ({
-  id: network.slug,
+  id: network.id,
   subject: 'network-avatar',
   path: `network/${network.id}/avatar`,
   convert: {width: 160, height: 160, fit: 'crop', rotate: 'exif'}
 })
 
 export const bannerUploadSettings = network => ({
-  id: network.slug,
+  id: network.id,
   subject: 'network-banner',
   path: `network/${network.id}/banner`,
   convert: {width: 1600, format: 'jpg', fit: 'max', rotate: 'exif'}

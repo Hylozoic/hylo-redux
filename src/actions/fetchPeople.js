58d5097def132a47d2a7d9343bea2446547d62d5
Lawrence Wang <l@lw.io>
2016-04-11 17:17:51 -0700

    cleanup

--- src/actions/fetchPeople.js
+++ src/actions/fetchPeople.js
@@ -5,27 +5,26 @@ export function fetchPeople (opts) {
   let { subject, id, limit, offset, search, cacheId } = opts
   if (!cacheId) cacheId = createCacheId(subject, id, {search})
   if (!offset) offset = 0
-  let payload = {api: true}
-  let cache = {id: cacheId, bucket: 'peopleByQuery', limit, offset, array: true}
-  var querystring
+  const cache = {id: cacheId, bucket: 'peopleByQuery', limit, offset, array: true}
+  let querystring, path
 
   switch (subject) {
     case 'community':
       querystring = cleanAndStringify({search, limit, offset})
-      payload.path = `/noo/community/${id}/members?${querystring}`
+      path = `/noo/community/${id}/members?${querystring}`
       break
     case 'project':
       querystring = cleanAndStringify({search, limit, offset, paginate: true})
-      payload.path = `/noo/project/${id}/users?${querystring}`
+      path = `/noo/project/${id}/users?${querystring}`
       break
     case 'network':
       querystring = cleanAndStringify({search, limit, offset})
-      payload.path = `/noo/network/${id}/members?${querystring}`
+      path = `/noo/network/${id}/members?${querystring}`
       break
     case 'voters':
-      payload.path = `/noo/post/${id}/voters`
+      path = `/noo/post/${id}/voters`
       break
   }
 
-  return {type: FETCH_PEOPLE, payload, meta: {cache}}
+  return {type: FETCH_PEOPLE, payload: {api: true, path}, meta: {cache}}
 }

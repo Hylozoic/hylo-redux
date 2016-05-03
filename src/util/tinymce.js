9c9051b1f77678763b065f2302dfafa2d5cac3db
Lawrence Wang <l@lw.io>
2016-05-02 18:35:32 -0700

    prepend text before tag in editor, not into it

--- src/util/tinymce.js
+++ src/util/tinymce.js
@@ -44,6 +44,20 @@ export const selectCurrentNode = editor => {
 }
 
 export const prepend = (text, editor) => {
-  editor.selection.setCursorLocation(null, 0)
+  const { selection } = editor
+  selection.setCursorLocation(null, 0)
+
+  if (editor.getContent() !== '') {
+    // add a space between the prepended text and existing content
+    text = text + ' '
+
+    // if there is a hashtag or mention at the beginning of the editor, we have
+    // to make sure we insert text before the tag, not inside it
+    const currentNode = selection.getNode()
+    if (currentNode.nodeName !== 'P') {
+      selection.setCursorLocation(currentNode.parentElement, 0)
+    }
+  }
+
   editor.execCommand('mceInsertContent', false, text, {skip_focus: true})
 }

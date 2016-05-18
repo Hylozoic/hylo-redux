b4d41c0541e2ee80d3eb82c7427c2239b12787d2
Lawrence Wang <l@lw.io>
2016-05-18 15:15:09 -0700

    remove search from projects list

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -46,13 +46,8 @@ const ProjectListControls = ({ onChange, filter, search }) => {
     {name: 'Projects I started or joined', id: 'mine'}
   ]
   const selectedFilter = filter ? filters.find(t => t.id === filter) : filters[0]
-  const delaySearch = debounce(search => onChange({search}), 500)
 
   return <div className='list-controls'>
-    <input type='text' className='form-control search'
-      placeholder='Search' defaultValue={search}
-      onChange={event => delaySearch(event.target.value)}/>
-
     <Select className='type' choices={filters} selected={selectedFilter}
       onChange={t => onChange({filter: t.id})} alignRight={true}/>
   </div>

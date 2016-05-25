04c6d8d9533a65fc0397cdce86eb13a054166ca6
Lawrence Wang <l@lw.io>
2016-05-25 11:54:06 -0700

    don't set isMobile for tablets

--- src/server/appHandler.js
+++ src/server/appHandler.js
@@ -38,7 +38,7 @@ export default function (req, res) {
   const routes = makeRoutes(store)
   const history = createHistory()
   const md = new MobileDetect(req.headers['user-agent'])
-  if (md.mobile()) store.dispatch(setMobileDevice())
+  if (md.mobile() && !md.tablet()) store.dispatch(setMobileDevice())
 
   return store.dispatch(fetchCurrentUser())
   .then(() => matchPromise({routes, location: req.originalUrl}))

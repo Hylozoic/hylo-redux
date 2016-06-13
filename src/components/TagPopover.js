65327a7f50fdb0a1661f3e3171705547dc7bff73
Robbie Carlton <robbie.carlton@gmail.com>
2016-06-13 14:56:23 -0700

    make popover look right

--- src/components/TagPopover.js
+++ src/components/TagPopover.js
@@ -4,8 +4,8 @@ import A from './A'
 import Avatar from './Avatar'
 import cx from 'classnames'
 import { isEmpty, get } from 'lodash'
-import { hideTagPopover, fetchTagSummary } from '../actions/index'
-const { string, object, func, array, number } = React.PropTypes
+import { hideTagPopover, fetchTagSummary, followTag } from '../actions/index'
+const { string, object, func, array, number, bool } = React.PropTypes
 
 const nounCount = (n, noun) => `${n} ${noun}${Number(n) !== 1 ? 's' : ''}`
 
@@ -15,6 +15,7 @@ const nounCount = (n, noun) => `${n} ${noun}${Number(n) !== 1 ? 's' : ''}`
     tagName,
     slug,
     position,
+    followed: get(tag, 'followed'),
     description: get(tag, 'description'),
     postCount: get(tag, 'post_count'),
     followerCount: get(tag, 'follower_count'),
@@ -31,7 +32,8 @@ export default class TagPopover extends React.Component {
     postCount: string,
     followerCount: number,
     activeMembers: array,
-    position: object
+    position: object,
+    followed: bool
   }
 
   hide () {
@@ -48,32 +50,36 @@ export default class TagPopover extends React.Component {
   }
 
   render () {
-    let { tagName, description, postCount, followerCount, activeMembers, position } = this.props
+    let { slug, tagName, description, postCount, followerCount,
+      activeMembers, position, followed, dispatch } = this.props
 
     if (isEmpty(tagName)) return null
+
+    const toggleFollow = () => dispatch(followTag(slug, tagName))
 
     return <div className={cx('popover')}
         style={{top: position.y, left: position.x}}
         ref='popover'
         onMouseLeave={() => this.hide()}>
         <div className='bottom-border'>
-          <span className='tag-name'>#{tagName}</span>
-          <span>{description}</span>
+          <span className='tag-name'># {tagName}</span>
+          <span className='description meta-text'>{description}</span>
         </div>
         {activeMembers !== undefined
-          ? <div className='bottom-border'>Most active members in this topic...</div>
-          : <div className='bottom-border'>Loading...</div>}
+          ? <div className='bottom-border meta-text'>Most active members in this topic...</div>
+          : <div className='bottom-border meta-text'>Loading...</div>}
         {activeMembers !== undefined && activeMembers.map(person =>
           <div className='bottom-border' key={person.id}>
-            <Avatar person={person}/><A to={`/u/${person.id}`}><strong className='name'>{person.name}</strong></A>
-            <span className='member-post-count'>
+            <Avatar person={person}/>
+            <A to={`/u/${person.id}`} className='name'><strong>{person.name}</strong></A><br />
+            <span className='member-post-count meta-text'>
               {nounCount(person.post_count, 'post')}
             </span>
-          </div>)}}
+          </div>)}
         <div className='bottom-border footer'>
-          <A to={'follow'}>Follow Topic</A> |
-          {followerCount && <span>{followerCount} following</span>}
-          {postCount && <span>{nounCount(postCount, 'post')}</span>}
+          <a onClick={toggleFollow}>{followed ? 'Unf' : 'F'}ollow Topic</a>
+          {followerCount && <span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{followerCount} following</span>}
+          {postCount && <span>&nbsp;&nbsp;&nbsp;{nounCount(postCount, 'post')}</span>}
         </div>
       </div>
   }

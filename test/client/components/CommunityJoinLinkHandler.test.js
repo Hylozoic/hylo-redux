09775c3765a63c1e163962e3679725bded1503ff
Robbie Carlton <robbie.carlton@gmail.com>
2016-06-22 13:53:51 -0700

    use wait

--- test/client/components/CommunityJoinLinkHandler.test.js
+++ test/client/components/CommunityJoinLinkHandler.test.js
@@ -5,13 +5,13 @@ import { renderIntoDocument } from 'react-addons-test-utils'
 import { NAVIGATE } from '../../../src/actions'
 const { createElement } = helpers
 
-describe.only('CommunityJoinLinkHandler', () => {
+describe('CommunityJoinLinkHandler', () => {
   var store, redirectUrl
 
   const renderAndWait = (props, callback) => {
     const component = createElement(CommunityJoinLinkHandler, props, {store})
     renderIntoDocument(component).getWrappedInstance()
-    setTimeout(callback, 10)
+    return helpers.wait(10, callback)
   }
 
   beforeEach(() => {
@@ -22,36 +22,33 @@ describe.only('CommunityJoinLinkHandler', () => {
     store.transformAction(NAVIGATE, action => redirectUrl = action.payload)
   })
 
-  it('redirects to onboarding with new membership', done => {
+  it('redirects to onboarding with new membership', () => {
     store.getState().people.current.memberships = [{
       community: {id: '1', name: 'Foomunity', slug: 'foomunity'}
     }]
-    renderAndWait({params: {id: 'foomunity', tagName: 'bar'}}, () => {
+    return renderAndWait({params: {id: 'foomunity', tagName: 'bar'}}, () => {
       expect(redirectUrl).to.equal('/c/foomunity/onboarding')
-      done()
     })
     // this is to compensate for the setTimeout in CommunityJoinLinkHandler
   })
 
-  it('redirects to community profile with existing membership', done => {
+  it('redirects to community profile with existing membership', () => {
     store.getState().people.current.memberships = [{
       community: {id: '1', name: 'Foomunity', slug: 'foomunity'},
       preexisting: true
     }]
-    renderAndWait({params: {id: 'foomunity'}}, () => {
+    return renderAndWait({params: {id: 'foomunity'}}, () => {
       expect(redirectUrl).to.equal('/c/foomunity')
-      done()
     })
   })
 
-  it('redirects to tag page with tagName param existing membership', done => {
+  it('redirects to tag page with tagName param existing membership', () => {
     store.getState().people.current.memberships = [{
       community: {id: '1', name: 'Foomunity', slug: 'foomunity'},
       preexisting: true
     }]
-    renderAndWait({params: {id: 'foomunity', tagName: 'bar'}}, () => {
+    return renderAndWait({params: {id: 'foomunity', tagName: 'bar'}}, () => {
       expect(redirectUrl).to.equal('/c/foomunity/tag/bar')
-      done()
     })
   })
 })

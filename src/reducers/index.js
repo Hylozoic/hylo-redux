be10219ec9519d079f73aa44c491912bb97d9f9e
Robbie Carlton <robbie.carlton@gmail.com>
2016-06-22 13:47:57 -0700

    move tagInvitationEditor to separate file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,6 +13,7 @@ import peopleByQuery from './peopleByQuery'
 import postEdits, { editingTagDescriptions, tagDescriptionEdits } from './postEdits'
 import postsByQuery from './postsByQuery'
 import { tagsByCommunity, tagsByQuery, totalTagsByQuery } from './tags'
+import tagInvitationEditor from './tagInvitationEditor'
 import posts from './posts'
 import {
   appendPayloadByPath, keyedCounter, keyedHasFreshItems, storePayload
@@ -64,7 +65,6 @@ import {
   UPDATE_COMMUNITY_EDITOR,
   UPDATE_NETWORK_EDITOR,
   UPDATE_INVITATION_EDITOR,
-  UPDATE_TAG_INVITATION_EDITOR,
   UPDATE_POST,
   UPLOAD_IMAGE,
   VALIDATE_COMMUNITY_ATTRIBUTE,
@@ -202,6 +202,7 @@ export default combineReducers({
   tagsByCommunity,
   tagsByQuery,
   tagDescriptionEdits,
+  tagInvitationEditor,
   thanks: appendPayloadByPath(FETCH_THANKS, 'meta.id'),
   totalActivities: keyedCounter(FETCH_ACTIVITY, 'total', 'meta.id'),
   totalCommunitiesByQuery: keyedCounter(FETCH_COMMUNITIES, 'communities_total'),
@@ -434,31 +435,6 @@ export default combineReducers({
       case UPDATE_INVITATION_EDITOR:
         return {...state, [payload.field]: payload.value}
       case SEND_COMMUNITY_INVITATION:
-        let { results } = payload
-        let [ failures, successes ] = partition(results, r => r.error)
-        let success, error
-        let sl = successes.length
-        if (sl > 0) {
-          let pl = sl > 1
-          success = `Sent invitation${pl ? 's' : ''} to ${sl} ${pl ? 'people' : 'person'}.`
-        }
-        if (some(failures)) {
-          error = failures.map(r => `Couldn't send to ${r.email}: ${r.error}.`).join(' ')
-        }
-        return {...state, success, error}
-    }
-    return state
-  },
-
-  tagInvitationEditor: (state = {}, action) => {
-    let { type, payload, error } = action
-    if (error) return state
-    switch (type) {
-      case UPDATE_TAG_INVITATION_EDITOR:
-        return {...state, [payload.field]: payload.value}
-      case CLOSE_MODAL:
-        return {}
-      case SEND_COMMUNITY_TAG_INVITATION:
         let { results } = payload
         let [ failures, successes ] = partition(results, r => r.error)
         let success, error

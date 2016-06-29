a48efba108ce672256a608ab379e772623b05ce7
Lawrence Wang <l@lw.io>
2016-06-29 12:46:43 -0700

    cleanup

--- src/models/community.js
+++ src/models/community.js
@@ -3,17 +3,17 @@ import { find, pickBy } from 'lodash/fp'
 
 export const MemberRole = {DEFAULT: 0, MODERATOR: 1}
 
-export const avatarUploadSettings = community => ({
-  id: community.slug,
+export const avatarUploadSettings = ({ id, slug }) => ({
+  id: slug,
   subject: 'community-avatar',
-  path: `community/${community.id}/avatar`,
+  path: `community/${id}/avatar`,
   convert: {width: 160, height: 160, fit: 'crop', rotate: 'exif'}
 })
 
-export const bannerUploadSettings = community => ({
-  id: community.slug,
+export const bannerUploadSettings = ({ id, slug }) => ({
+  id: slug,
   subject: 'community-banner',
-  path: `community/${community.id}/banner`,
+  path: `community/${id}/banner`,
   convert: {width: 1600, format: 'jpg', fit: 'max', rotate: 'exif'}
 })
 
@@ -23,5 +23,5 @@ export const getCommunity = (idOrSlug, state) =>
 export const getCurrentCommunity = state =>
   getCommunity(state.currentCommunityId, state)
 
-export const getFollowedTags = (community, state) =>
-  values(pickBy(t => t.followed, state.tagsByCommunity[community.slug]))
+export const getFollowedTags = ({ slug }, state) =>
+  values(pickBy('followed', state.tagsByCommunity[slug]))

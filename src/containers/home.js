2dbae5f4ea9fd67f9537c1f8f2160fe251d37475
Robbie Carlton <robbie.carlton@gmail.com>
2016-06-30 16:51:53 -0700

    redirect to community with currentCommunityId

--- src/containers/home.js
+++ src/containers/home.js
@@ -5,19 +5,44 @@ import { connect } from 'react-redux'
 import { fetch, ConnectedPostList } from './ConnectedPostList'
 import PostEditor from '../components/PostEditor'
 import CoverImagePage from '../components/CoverImagePage'
+import { navigate, fetchCommunity } from '../actions'
 import { setCurrentCommunityIdLocalAndRemote } from '../actions/util'
+import { communityUrl } from '../routes'
+const { object } = React.PropTypes
 
-const makeComponent = (subject, title, showEditor) => props => {
-  const { location: { query }, currentUser: { id } } = props
+const makeComponent = (subject, title, showEditor) => React.createClass({
+  propTypes: {
+    location: object,
+    currentUser: object
+  },
 
-  return <CoverImagePage>
-    {showEditor && <PostEditor/>}
-    <ConnectedPostList {...{subject, id, query}}/>
-  </CoverImagePage>
-}
+  componentDidMount: function () {
+    const { query: { rd }, pathname } = this.props.location
+    rd && window.history.replaceState({}, 'Hylo', pathname)
+  },
+
+  render: function () {
+    const { location: { query }, currentUser: { id } } = this.props
+
+    return <CoverImagePage>
+      {showEditor && <PostEditor/>}
+      <ConnectedPostList {...{subject, id, query}}/>
+    </CoverImagePage>
+  }
+})
 
 const wrapComponent = (subject, ...args) => compose(
-  prefetch(({ dispatch, params, currentUser: { id }, query }) => {
+  prefetch(({ dispatch, params, currentUser: { id, settings: { currentCommunityId } }, query, path }) => {
+    if (query.rd && currentCommunityId) {
+      if (currentCommunityId !== 'all') {
+        // we only have the id, so have to fetch to get the slug
+        return dispatch(fetchCommunity(currentCommunityId))
+        .then(action => {
+          if (action.error) return
+          return dispatch(navigate(communityUrl(action.payload)))
+        })
+      }
+    }
     setCurrentCommunityIdLocalAndRemote(dispatch, 'all', id)
     return dispatch(fetch(subject, id, query))
   }),

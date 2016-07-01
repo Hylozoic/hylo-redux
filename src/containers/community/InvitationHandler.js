cf839e9a4d8e5674fe7c257f5ef317242b0eb779
Lawrence Wang <l@lw.io>
2016-07-01 15:29:01 -0700

    fix same race condition for InvitationHandler
    also remove “already a member” error because in this case we just
    redirect to the community

--- src/containers/community/InvitationHandler.js
+++ src/containers/community/InvitationHandler.js
@@ -2,44 +2,21 @@ import React from 'react'
 import { prefetch } from 'react-fetcher'
 import { connect } from 'react-redux'
 import { get } from 'lodash'
-import {
-  USE_INVITATION,
-  useInvitation,
-  navigate,
-  fetchCommunity,
-  setCurrentCommunityId
-} from '../../actions'
-import { fetchLeftNavTags } from '../../actions/tags'
+import { USE_INVITATION, useInvitation, navigate } from '../../actions'
 const { func, object, string } = React.PropTypes
 
-@prefetch(({ path, query: { token }, dispatch }) =>
-  dispatch(useInvitation(token)))
+@prefetch(({ path, query: { token }, store, dispatch }) =>
+  dispatch(useInvitation(token))
+  .then(({ error, payload }) =>
+    error || dispatch(navigate(`/c/${payload.community.slug}/onboarding`))))
 @connect(({ errors, people, communities }, { location: { query: { token } } }) => ({
-  tokenError: get(errors[USE_INVITATION], 'payload.response.body'),
-  currentUser: people.current,
-  community: communities[token]
+  tokenError: get(errors[USE_INVITATION], 'payload.response.body')
 }))
 export default class InvitationHandler extends React.Component {
   static propTypes = {
-    currentUser: object,
     dispatch: func,
     params: object,
-    tokenError: string,
-    community: object
-  }
-
-  // FIXME this kind of smells. at first, I tried doing a navigate in the
-  // prefetch after the join action finishes, and this works on the client, but
-  // not in server-side rendering. would have to jump back into appHandler to
-  // implement checking for redirects that take place during prefetching
-  componentDidMount () {
-    let { dispatch, community: { slug, id } } = this.props
-    if (slug) {
-      dispatch(fetchLeftNavTags(slug))
-      .then(() => dispatch(fetchCommunity(slug)))
-      .then(() => dispatch(setCurrentCommunityId(id)))
-      .then(() => dispatch(navigate(`/c/${slug}`)))
-    }
+    tokenError: string
   }
 
   render () {
@@ -51,9 +28,6 @@ export default class InvitationHandler extends React.Component {
         break
       case 'used token':
         errorMessage = 'This invitation link has already been used.'
-        break
-      case 'already a member':
-        errorMessage = `You are already a member of the community.`
         break
     }
 
---
+++ test/components/InvitationHandler.test.js
@@ -0,0 +1,58 @@
+import { mocks, helpers } from '../support'
+import InvitationHandler from '../../src/containers/community/InvitationHandler'
+import { renderToString } from 'react-dom/server'
+import cheerio from 'cheerio'
+import { NAVIGATE, USE_INVITATION } from '../../src/actions'
+import { getPrefetchedData } from 'react-fetcher'
+const { createElement } = helpers
+
+const mockPrefetch = ({ dispatch }, query) =>
+  getPrefetchedData([InvitationHandler], {query, dispatch})
+
+describe('InvitationHandler', () => {
+  var store
+
+  describe('when successful', () => {
+    var redirectUrl
+
+    beforeEach(() => {
+      store = mocks.redux.store()
+      store.transformAction(NAVIGATE, ({ payload }) => redirectUrl = payload)
+    })
+
+    it('redirects', () => {
+      store.transformAction(USE_INVITATION, action => {
+        expect(action.payload.path).to.equal('/noo/invitation/zap')
+        return Promise.resolve({
+          payload: {
+            community: {id: 1, name: 'Foo', slug: 'foo'}
+          }
+        })
+      })
+
+      return mockPrefetch(store, {token: 'zap'})
+      .then(() => expect(redirectUrl).to.equal('/c/foo/onboarding'))
+    })
+  })
+
+  describe('when unsuccessful', () => {
+    var props
+    beforeEach(() => {
+      store = mocks.redux.store({
+        errors: {
+          [USE_INVITATION]: {payload: {response: {body: 'used token'}}}
+        }
+      })
+      props = {
+        location: {query: {token: 'zap'}}
+      }
+    })
+
+    it('shows an error message if invitation use fails', () => {
+      const component = createElement(InvitationHandler, props, {store})
+      const node = renderToString(component)
+      const doc = cheerio.load(node)
+      expect(doc('.alert').text()).to.equal('This invitation link has already been used.')
+    })
+  })
+})

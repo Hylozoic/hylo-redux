204eb2289245c4d65308d35a6ea53e93137a9d71
Lawrence Wang <l@lw.io>
2016-07-18 13:45:19 -0700

    style fixes
    fix a bad margin in mobile post editor and lay out previews that don’t
    have images and/or descriptions better

--- src/components/LinkPreview.js
+++ src/components/LinkPreview.js
@@ -6,13 +6,14 @@ const LinkPreview = ({ linkPreview, onClose }) => {
     event.preventDefault()
     onClose(event)
   }
+  const imageClassName = image_width < 800 ? 'small' : ''
 
   return <div className='post-section link-preview-section'>
     <a className='link-preview' href={url} target='_blank'>
       {onClose && <div className='close' onClick={close}>&times;</div>}
-      <img src={image_url} className={image_width < 800 ? 'small' : ''}/>
+      {image_url && <img src={image_url} className={imageClassName}/>}
       <span className='link-preview-title'>{title}</span>
-      <span className='link-preview-description'>{description}</span>
+      {description && <span className='link-preview-description'>{description}</span>}
     </a>
   </div>
 }

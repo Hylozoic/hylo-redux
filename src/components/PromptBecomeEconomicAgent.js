4d056073c0250cec75c8479c727651219525371a
Maggie Zong <mzong@thoughtworks.com>
2016-08-22 15:28:53 +1000

    CW/MZ - #25 made the promptText to match AC and can link to Settings page

--- src/components/PromptBecomeEconomicAgent.js
+++ src/components/PromptBecomeEconomicAgent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 class PromptBecomeEconomicAgent extends React.Component {
 
@@ -8,9 +9,13 @@ class PromptBecomeEconomicAgent extends React.Component {
 
   render () {
     const { thing } = this.props
-
+    const promptText = "Please connect a HitFin account to pledge funds to projects. You can do this in the Payment Details section of your Account Settings"
     return(
-      <div className='prompt-become-economic-agent'>Connect your account</div>
+      <div className='prompt-become-economic-agent'>
+        <Link to='/settings'>
+          {promptText}
+        </Link>
+      </div>
     )
   }
 }

449b820a3158aef6c0294a698d0fc554655d4ed1
Lawrence Wang <l@lw.io>
2016-08-24 13:28:17 -0700

    add new relic browser tracking snippet

--- src/containers/Html.js
+++ src/containers/Html.js
@@ -6,10 +6,14 @@ var { string, object } = React.PropTypes
 
 const isProd = environment === 'production'
 
+const rawScript = contents =>
+  <script type='text/javascript' dangerouslySetInnerHTML={{__html: contents}}></script>
+
 const Html = props => {
-  const { pageTitle, markup, state, assetManifest, metaTags } = props
+  const { pageTitle, markup, initNewRelic, state, assetManifest, metaTags } = props
   return <html>
     <head>
+      {initNewRelic && rawScript(initNewRelic)}
       <title>{pageTitle}</title>
       <link rel='stylesheet' type='text/css' href={assetUrl('/index.css')}/>
       <link rel='shortcut icon' id='favicon' href={assetUrl(`/img/favicon${isProd ? '' : 'Dev'}.png?v=2`)}/>
@@ -22,8 +26,8 @@ const Html = props => {
     <body>
       <div id='app' dangerouslySetInnerHTML={{__html: markup}}></div>
       <script src='//cdn.tinymce.com/4/tinymce.min.js'></script>
-      <script dangerouslySetInnerHTML={{__html: state}}></script>
-      <script dangerouslySetInnerHTML={{__html: assetManifest}}></script>
+      {rawScript(state)}
+      {rawScript(assetManifest)}
       <script src={assetUrl('/index.js')} defer></script>
     </body>
   </html>
@@ -34,7 +38,8 @@ Html.propTypes = {
   markup: string,
   state: string,
   assetManifest: string,
-  metaTags: object
+  metaTags: object,
+  initNewRelic: string
 }
 
 export default Html

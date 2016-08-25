c805a97ae3c88001b3815fcbb26a108afafe16f0
Lawrence Wang <l@lw.io>
2016-08-25 14:23:03 -0700

    revise LazyLoader to attach event listeners only once

--- src/components/LazyLoader.js
+++ src/components/LazyLoader.js
@@ -2,6 +2,44 @@ import React from 'react'
 import { debounce } from 'lodash'
 const { func, node } = React.PropTypes
 
+var loaders
+
+const register = (loader, onShow) => {
+  loaders.push({loader, onShow})
+  checkOne(loader, onShow)
+}
+
+const unregister = (loader) => {
+  if (!loaders) return // this happens during hot reloading
+  loaders = loaders.filter(x => x.loader !== loader)
+}
+
+const checkOne = (loader, onShow) => {
+  const height = document.documentElement.clientHeight
+  const { top, bottom } = loader.getBoundingClientRect()
+  if ((top > 0 && top < height) || (bottom > 0 && bottom < height)) {
+    onShow()
+    unregister(loader)
+  }
+}
+
+const check = debounce(() => {
+  const height = document.documentElement.clientHeight
+  loaders.forEach(({ loader, onShow }) => {
+    const { top, bottom } = loader.getBoundingClientRect()
+    if ((top > 0 && top < height) || (bottom > 0 && bottom < height)) {
+      onShow()
+      unregister(loader)
+    }
+  })
+}, 200)
+
+export const init = () => {
+  loaders = []
+  window.addEventListener('resize', check)
+  window.addEventListener('scroll', check)
+}
+
 export default class LazyLoader extends React.Component {
   static propTypes = {
     children: node,
@@ -13,35 +51,21 @@ export default class LazyLoader extends React.Component {
     this.state = {}
   }
 
-  checkVisibility = debounce(() => {
+  componentDidMount () {
     const { onContentVisible } = this.props
-    const height = document.documentElement.clientHeight
-    const { top, bottom } = this.refs.loader.getBoundingClientRect()
-
-    if ((top > 0 && top < height) || (bottom > 0 && bottom < height)) {
+    register(this.refs.loader, () => {
       if (onContentVisible) onContentVisible()
       this.setState({visible: true})
-      window.removeEventListener('resize', this.checkVisibility)
-      window.removeEventListener('scroll', this.checkVisibility)
-    }
-  })
-
-  componentDidMount () {
-    window.addEventListener('resize', this.checkVisibility)
-    window.addEventListener('scroll', this.checkVisibility)
-    this.checkVisibility()
+    })
   }
 
   componentWillUnmount () {
-    window.removeEventListener('resize', this.checkVisibility)
-    window.removeEventListener('scroll', this.checkVisibility)
+    unregister(this.refs.loader)
   }
 
   render () {
-    const { visible } = this.state
-    const { children } = this.props
     return <div className='lazy-loader' ref='loader'>
-      {visible && children}
+      {this.state.visible && this.props.children}
     </div>
   }
 }

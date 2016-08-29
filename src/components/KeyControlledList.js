bb74f0b1d0b629c0fe340ac5f1ceb199863e3f2a
Robbie Carlton <robbie.carlton@gmail.com>
2016-08-29 16:04:16 -0600

    make KeyControlledList depend on GenericKeyControlledList

--- src/components/KeyControlledList.js
+++ src/components/KeyControlledList.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import cx from 'classnames'
 import { indexOf, isEmpty } from 'lodash'
+import { find } from 'lodash/fp'
 import { getKeyCode, keyMap } from '../util/textInput'
 var { array, func, object } = React.PropTypes
 
@@ -12,50 +12,9 @@ export default class KeyControlledList extends React.Component {
     selected: object
   }
 
-  constructor (props) {
-    super(props)
-    let { items, selected } = props
-    let index = indexOf(items, selected)
-    this.state = {selectedIndex: index !== -1 ? index : 0}
-  }
-
-  componentWillReceiveProps (nextProps) {
-    var max = nextProps.items.length - 1
-    if (this.state.selectedIndex > max) {
-      this.setState({selectedIndex: max})
-    }
-  }
-
-  changeSelection = delta => {
-    if (isEmpty(this.props.items)) return
-
-    var i = this.state.selectedIndex
-    var length = this.props.items.length
-
-    i += delta
-    if (i < 0) i += length
-    i = i % length
-
-    this.setState({selectedIndex: i})
-  }
-
   // this method is called from other components
   handleKeys = event => {
-    switch (getKeyCode(event)) {
-      case keyMap.UP:
-        this.changeSelection(-1)
-        return true
-      case keyMap.DOWN:
-        this.changeSelection(1)
-        return true
-      case keyMap.TAB:
-      case keyMap.ENTER:
-        if (!isEmpty(this.props.items)) {
-          var choice = this.props.items[this.state.selectedIndex]
-          this.change(choice, event)
-        }
-        return true
-    }
+    this.refs.gkcl.handleKeys(event)
   }
 
   change = (choice, event) => {
@@ -63,17 +22,22 @@ export default class KeyControlledList extends React.Component {
     this.props.onChange(choice, event)
   }
 
+  onChangeExtractingItem = (choice, event) => {
+    const item = find(i => i.id === choice.key, this.props.items)
+    this.change(item, event)
+  }
+
   // FIXME use more standard props e.g. {label, value} instead of {id, name}, or
   // provide an API for configuring them
   render () {
-    let { selectedIndex } = this.state
     let { items, onChange, ...props } = this.props
-    return <ul {...props}>
+    return <GenericKeyControlledList ref='gkcl'
+      onChange={this.onChangeExtractingItem} {...props}>
       {items.map((c, i) =>
-        <li key={c.id || 'blank'} className={cx({active: selectedIndex === i})}>
+        <li key={c.id || 'blank'}>
           <a onClick={event => this.change(c, event)}>{c.name}</a>
         </li>)}
-    </ul>
+    </GenericKeyControlledList>
   }
 }
 

81894b2fb998d8dbb238bae1c5eae80bea9e085e
Lawrence Wang <l@lw.io>
2016-09-01 15:20:59 -0700

    style typing notification

--- src/components/PeopleTyping.js
+++ src/components/PeopleTyping.js
@@ -1,8 +1,7 @@
 import React from 'react'
-const { array, bool, func, object, string } = React.PropTypes
+const { array, bool } = React.PropTypes
 
 export default class PeopleTyping extends React.Component {
-
   static propTypes = {
     names: array,
     showNames: bool
@@ -11,10 +10,18 @@ export default class PeopleTyping extends React.Component {
   render () {
     const { names, showNames } = this.props
     return <div className='typing'>
-      {!showNames && names.length == 1 && <div>someone is typing</div>}
-      {showNames && names.length == 1 && <div>{names[0]} is typing</div>}
-      {names.length > 1 && <div>multiple people are typing</div>}
+      <Chillipsis/>
+      {!showNames && names.length === 1 && <div>Someone is typing...</div>}
+      {showNames && names.length === 1 && <div>{names[0]} is typing...</div>}
+      {names.length > 1 && <div>Multiple people are typing...</div>}
     </div>
   }
+}
 
+const Chillipsis = () => {
+  return <div className='chillipsis'>
+    <div className='dot d1'></div>
+    <div className='dot d2'></div>
+    <div className='dot d3'></div>
+  </div>
 }

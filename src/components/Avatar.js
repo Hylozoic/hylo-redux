22632969f333c94d50d404af8dcc061688cd3e35
Lawrence Wang <l@lw.io>
2016-09-12 15:23:41 -0700

    escape parentheses in avatars

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+const bgStyle = url => {
+  if (!url) return {}
+
+  const escaped = url.replace(/([\(\)])/g, (match, $1) => '\\' + $1)
+  return {backgroundImage: `url(${escaped})`}
+}
+
 const Avatar = ({ person: { id, avatar_url } }) => {
   if (!id) return <span></span>
 
-  return <Link to={`/u/${id}`} className='avatar'
-    style={{backgroundImage: `url(${avatar_url})`}}/>
+  return <Link to={`/u/${id}`} className='avatar' style={bgStyle(avatar_url)}/>
 }
 
 export const NonLinkAvatar = ({ person: { id, avatar_url } }) => {
   if (!id) return <span></span>
-  return <div className='avatar'
-    style={{backgroundImage: `url(${avatar_url})`}}/>
+  return <div className='avatar' style={bgStyle(avatar_url)}/>
 }
 
 export default Avatar

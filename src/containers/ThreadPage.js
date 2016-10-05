ecbc12907000942e5620ed936fbd1824cc80f536
Lawrence Wang <l@lw.io>
2016-10-04 18:34:03 -0700

    a bit more cleanup

--- src/containers/ThreadPage.js
+++ src/containers/ThreadPage.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import { prefetch } from 'react-fetcher'
 import { connect } from 'react-redux'
-import { get, includes } from 'lodash'
 import { pick } from 'lodash/fp'
-import {
-  FETCH_POST, navigate, setMetaTags
-} from '../actions'
+import { FETCH_POST } from '../actions'
 import { fetchComments } from '../actions/comments'
 import { fetchPost } from '../actions/posts'
-import { ogMetaTags } from '../util'
-import A from '../components/A'
 import { findError } from '../actions/util'
 import AccessErrorMessage from '../components/AccessErrorMessage'
 import Thread from '../components/Thread'
 import { denormalizedPost, getComments, getPost } from '../models/post'
-const { array, bool, object, string, func } = React.PropTypes
-
+const { array, bool, object, string } = React.PropTypes
 
 @prefetch(({ store, dispatch, params: { id }, query }) =>
   dispatch(fetchPost(id))
-  .then(action => setupPage(store, id, query, action)))
+  .then(({ error, payload }) => !error && payload && fetchComments(id)))
 @connect((state, { params: { id } }) => {
   const post = getPost(id, state)
   return {
@@ -50,30 +44,9 @@ export default class ThreadPage extends React.Component {
 
   render () {
     const { post, error } = this.props
-    const { currentUser, isMobile } = this.context
     if (error) return <AccessErrorMessage error={error}/>
     if (!post || !post.user) return <div className='loading'>Loading...</div>
 
     return <Thread post={post} />
   }
 }
-
-const setupPage = (store, id, query, action) => {
-  const { error, payload, cacheHit } = action
-  const { dispatch } = store
-  if (error) return
-  const state = store.getState()
-  const post = state.posts[id]
-  if (!post) return
-
-  const communityId = get(post, 'communities.0') || 'all'
-  const userId = get(state.people, 'current.id')
-
-  if (payload && !payload.api) {
-    const { name, description, media } = payload
-    dispatch(setMetaTags(ogMetaTags(name, description, get(media, '0'))))
-  }
-
-  return dispatch(fetchComments(id))
-}
-

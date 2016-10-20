1dcc0987fc96e54d75ff7cd8b34a4f577fe7ebd8
Lawrence Wang <l@lw.io>
2016-10-19 17:34:25 -0700

    add some simple tests for Comment

---
+++ test/client/components/Comment.test.js
@@ -0,0 +1,49 @@
+require('../support')
+import { mount } from 'enzyme'
+import React from 'react'
+import Comment from '../../../src/components/Comment'
+import { configureStore } from '../../../src/store'
+const { object } = React.PropTypes
+
+const currentUser = {id: '5', name: 'me'}
+
+describe('Comment', () => {
+  var comment, store, node
+
+  beforeEach(() => {
+    comment = {id: '1', text: 'yes!', user: {id: '6', name: 'jo'}}
+  })
+
+  const render = () => {
+    store = configureStore({
+      people: {current: currentUser},
+      comments: {[comment.id]: comment}
+    }).store
+
+    node = mount(<Comment comment={comment}/>, {
+      context: {store, dispatch: store.dispatch, currentUser},
+      childContextTypes: {store: object, currentUser: object}
+    })
+  }
+
+  it('behaves as expected', () => {
+    render()
+    expect(node.find('.content .text').text()).to.equal('jo' + 'yes!')
+    expect(node.find('.content .text').html()).to.include('<strong class="name">jo</strong>')
+    expect(node.find('.content > div > span a').text()).to.equal('Say thanks')
+
+    node.find('a.thanks').simulate('click')
+    node.setProps({comment: store.getState().comments[comment.id]})
+    expect(node.find('.content > div > span a').text()).to.equal('You thanked jo')
+  })
+
+  it('allows editing', () => {
+    comment.user = currentUser
+    render()
+
+    node.find('.dropdown-toggle').simulate('click')
+    expect(node.find('.dropdown li a').length).to.equal(2)
+    node.find('.dropdown li a').first().simulate('click')
+    expect(node.find('.comment-form').length).to.equal(1)
+  })
+})

ea21f1d592db26fb8ac4b2ebd20d0ff7df5e7eb7
Robbie Carlton <robbie.carlton@gmail.com>
2016-10-28 15:41:29 -0600

    test for double tag-editor bug

--- test/client/components/CommentForm.test.js
+++ test/client/components/CommentForm.test.js
@@ -1,71 +1,67 @@
 require('../support')
-import { mocks, helpers } from '../../support'
-import ConnectedPostList from '../../../src/containers/ConnectedPostList'
-import {
-  findRenderedDOMComponentWithClass,
-  findRenderedDOMComponentWithTag,
-  renderIntoDocument,
-  Simulate
-} from 'react-addons-test-utils'
-const { click, change } = Simulate
+import { mount } from 'enzyme'
+import React from 'react'
+import CommentForm from '../../../src/components/CommentForm'
+import CREATE_COMMENT from '../../../src/actions'
+import { configureStore } from '../../../src/store'
+const { object, func } = React.PropTypes
 
-describe('CommentForm', () => {
-  describe('in ConnectedPostList', () => {
-    let community = {id: '1', name: 'Foomunity', slug: 'foo'}
+describe.only('CommentForm', () => {
+  let community = {id: '1', name: 'Foomunity', slug: 'foo'}
 
-    let currentUser = {
-      id: 'x',
-      name: 'Mr. X',
-      avatar_url: '/img/mrx.png'
+  let currentUser = {
+    id: 'x',
+    name: 'Mr. X',
+    avatar_url: '/img/mrx.png'
+  }
+
+  let state = {
+    communities: {
+      [community.slug]: community
+    },
+    people: {
+      current: currentUser
+    },
+    posts: {
+      '1': {
+        id: '1', name: 'post', type: 'offer',
+        user_id: 'x',
+        communities: [community.id],
+        description: '<p>.</p>'
+      }
+    },
+    commentEdits: {
+      new: {
+        '1': 'comment text'
+      }
     }
+  }
 
-    let state = {
-      communities: {
-        [community.slug]: community
-      },
-      people: {
-        current: currentUser
-      },
-      posts: {
-        '1': {
-          id: '1', name: 'post', type: 'offer',
-          user: {id: 1, name: 'jo', avatar_url: ''},
-          communities: [community],
-          description: '<p>A long enough description that the show more button is displayed.</p>' +
-            '<p>A long enough description that the show more button is displayed.</p>' +
-            '<p>A long enough description that the show more button is displayed.</p>' +
-            '<p>A long enough description that the show more button is displayed.</p>' +
-            '<p>A long enough description that the show more button is displayed.</p>' +
-            '<p>A long enough description that the show more button is displayed.</p>'
-        }
-      },
-      totalPostsByQuery: {'subject=community&id=foo': 1},
-      postsByQuery: {'subject=community&id=foo': [1]},
-      pending: {}
-    }
-
-    it.skip('retains comment data after blur', () => {
-      const props = {subject: 'community', id: 'foo'}
-      const store = mocks.redux.store(state)
-      const context = {store, community, currentUser}
-      const commentText = 'the partially written comment'
-      let component = helpers.createElement(ConnectedPostList, props, context)
-      let node = renderIntoDocument(component).getWrappedInstance()
-      let showMoreButton = findRenderedDOMComponentWithClass(node, 'show-more')
-      click(showMoreButton)
-      let placeholder = findRenderedDOMComponentWithClass(node, 'content placeholder')
-      click(placeholder)
-      let textArea = findRenderedDOMComponentWithTag(node, 'textarea')
-      textArea.value = commentText
-      change(textArea)
-      let backdrop = findRenderedDOMComponentWithClass(node, 'backdrop')
-      click(backdrop)
-      showMoreButton = findRenderedDOMComponentWithClass(node, 'show-more')
-      click(showMoreButton)
-      placeholder = findRenderedDOMComponentWithClass(node, 'content placeholder')
-      click(placeholder)
-      textArea = findRenderedDOMComponentWithTag(node, 'textarea')
-      expect(textArea.value).to.equal(commentText)
+  it('posts updated tagDescriptions when saveWithTagDescriptions is called', () => {
+    var createCommentParams
+    const store = configureStore(state).store
+    store.oldDispatch = store.dispatch
+    store.dispatch = function (action) {
+      console.log('DISPATCHING:', action)
+      if (action.type === CREATE_COMMENT) {
+        createCommentParams = action.payload.params
+      }
+      return this.oldDispatch(action)
+    }.bind(store)
+    const node = mount(<CommentForm postId={'1'}/>, {
+      context: {store, dispatch: store.dispatch, currentUser: state.people.current},
+      childContextTypes: {store: object, currentUser: object, dispatch: func}
+    })
+    const wrappedInstance = node.instance().getWrappedInstance()
+    wrappedInstance.setState({enabled: true})
+    wrappedInstance.saveWithTagDescriptions({
+      thenewtag: {description: 'something', is_default: false}
+    })
+    expect(createCommentParams).to.deep.equal({
+      text: 'comment text',
+      tagDescriptions: {
+        thenewtag: {description: 'something', is_default: false}
+      }
     })
   })
 })

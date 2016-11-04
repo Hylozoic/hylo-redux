6aadcea835941acf3267dcfa3cdfe6d342428f49
Lawrence Wang <l@lw.io>
2016-11-04 12:36:07 -0700

    clean up linter warnings

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import Avatar from './Avatar'
 import A from './A'
-import { some } from 'lodash'
-import { get } from 'lodash/fp'
-import { humanDate, prependInP, present, textLength } from '../util/text'
+import { humanDate, present } from '../util/text'
 import { sanitize } from 'hylo-utils/text'
-import { commentUrl } from '../routes'
-import truncateHtml from 'trunc-html'
-var { func, object, bool } = React.PropTypes
-
-const spacer = <span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+var { func, object } = React.PropTypes
 
 class Message extends React.Component {
   static propTypes = {
@@ -23,7 +17,6 @@ class Message extends React.Component {
 
   render () {
     const { message } = this.props
-    const { dispatch, currentUser } = this.context
 
     const person = message.user
     let text = present(sanitize(message.text))
@@ -36,7 +29,7 @@ class Message extends React.Component {
           <A to={`/u/${person.id}`}><strong className='name'>{sanitize(person.name)}</strong></A>
           <span className='date'>{humanDate(message.created_at)}</span>
         </div>
-        <div className='text'> 
+        <div className='text'>
           <span dangerouslySetInnerHTML={{__html: text}}/>
         </div>
       </div>
@@ -45,4 +38,3 @@ class Message extends React.Component {
 }
 
 export default Message
-
--- src/components/MessageSection.js
+++ src/components/MessageSection.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { throttle, isEmpty, sortBy, values } from 'lodash'
-const { array, bool, string, func, object } = React.PropTypes
+import { throttle, isEmpty, sortBy } from 'lodash'
+const { array, func, object } = React.PropTypes
 import cx from 'classnames'
 import Message from './Message'
-import { appendComment } from '../actions/comments'
-import { getSocket, socketUrl } from '../client/websockets'
 
 export default class MessageSection extends React.Component {
   static propTypes = {
@@ -16,37 +14,36 @@ export default class MessageSection extends React.Component {
     dispatch: func
   }
 
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       scrolledUp: false
     }
   }
 
-  componentDidMount() {
-    this._scrollToBottom()  
+  componentDidMount () {
+    this._scrollToBottom()
   }
 
-  componentDidUpdate() {
+  componentDidUpdate () {
     const { scrolledUp } = this.state
     if (!scrolledUp) this._scrollToBottom()
   }
 
-  _handleScroll(target) {
+  _handleScroll (target) {
     const { scrolledUp } = this.state
-    const onBottom = target.scrollTop > target.scrollHeight - target.offsetHeight 
+    const onBottom = target.scrollTop > target.scrollHeight - target.offsetHeight
     if (!onBottom && !scrolledUp) this.setState({ scrolledUp: true })
     else if (onBottom && scrolledUp) this.setState({ scrolledUp: false })
   }
 
-  _scrollToBottom() {
+  _scrollToBottom () {
     const div = this.refs['messageList']
     div.scrollTop = div.scrollHeight
   }
 
   render () {
     let { messages } = this.props
-    const { currentUser } = this.context
     const throttledScroll = throttle(this._handleScroll.bind(this), 500, { trailing: true })
     const onScroll = (event) => {
       // must be done this way because of event pooling https://fb.me/react-event-pooling
@@ -57,12 +54,10 @@ export default class MessageSection extends React.Component {
     if (!messages) messages = []
     messages = sortBy(messages, m => m.created_at)
 
-    return <div
-             className={cx('messages-section', {empty: isEmpty(messages)})}
-             ref='messageList'
-             onScroll={onScroll}>
+    return <div className={cx('messages-section', {empty: isEmpty(messages)})}
+     ref='messageList'
+     onScroll={onScroll}>
       {messages.map(m => <Message message={m} key={m.id}/>)}
     </div>
   }
 }
-

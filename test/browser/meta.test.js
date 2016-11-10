842c970d7088d15c5d3c49225773c0d8af30c0fb
Lawrence Wang <l@lw.io>
2016-11-09 17:37:07 -0800

    increase timeout for slow test

--- test/browser/meta.test.js
+++ test/browser/meta.test.js
@@ -17,7 +17,7 @@ describe('browser testing setup', () => {
 
   describe('rendering a React component', () => {
     before(function () {
-      this.timeout(5000)
+      this.timeout(10000)
       return useTestFileScaffold('meta')
     })
     after(() => removeTestFile())

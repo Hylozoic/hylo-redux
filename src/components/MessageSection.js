27a70d0a18c79a5eccaaca90559c401a544ea0fc
Lawrence Wang <l@lw.io>
2016-11-11 17:11:51 -0800

    add onScrollToTop and scrollToMessage to MessageSection
    for detecting when the component has scrolled all the way up, and for
    jumping back to the last-seen comment after new ones have been loaded

--- src/components/MessageSection.js
+++ src/components/MessageSection.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { throttle, isEmpty, sortBy } from 'lodash'
-const { array, func, object } = React.PropTypes
+const { array, bool, func, object } = React.PropTypes
 import cx from 'classnames'
 import Message from './Message'
+import { position } from '../util/scrolling'
+import { findDOMNode } from 'react-dom'
 
 export default class MessageSection extends React.Component {
   static propTypes = {
-    messages: array
+    messages: array,
+    onScrollToTop: func,
+    pending: bool
   }
 
   static contextTypes = {
@@ -30,34 +34,37 @@ export default class MessageSection extends React.Component {
     if (!scrolledUp) this._scrollToBottom()
   }
 
-  _handleScroll (target) {
-    const { scrolledUp } = this.state
-    const onBottom = target.scrollTop > target.scrollHeight - target.offsetHeight
-    if (!onBottom && !scrolledUp) this.setState({ scrolledUp: true })
-    else if (onBottom && scrolledUp) this.setState({ scrolledUp: false })
+  scrollToMessage (id) {
+    const message = findDOMNode(this['message' + id])
+    const messageTop = position(message, this.list).y -
+      document.querySelector('.thread .header').offsetHeight -
+      document.getElementById('topNav').offsetHeight - 40
+    this.list.scrollTop = messageTop
   }
 
+  _handleScroll = throttle(target => {
+    const { scrolledUp } = this.state
+    const { scrollTop, scrollHeight, offsetHeight } = target
+    const onBottom = scrollTop > scrollHeight - offsetHeight
+    if (!onBottom && !scrolledUp) this.setState({scrolledUp: true})
+    else if (onBottom && scrolledUp) this.setState({scrolledUp: false})
+
+    if (scrollTop <= 20 && this.props.onScrollToTop) this.props.onScrollToTop()
+  }, 500, {trailing: true})
+
   _scrollToBottom () {
-    const div = this.refs['messageList']
-    div.scrollTop = div.scrollHeight
+    this.list.scrollTop = this.list.scrollHeight
   }
 
   render () {
-    let { messages } = this.props
-    const throttledScroll = throttle(this._handleScroll.bind(this), 500, { trailing: true })
-    const onScroll = (event) => {
-      // must be done this way because of event pooling https://fb.me/react-event-pooling
-      const { target } = event
-      throttledScroll(target)
-    }
-
-    if (!messages) messages = []
-    messages = sortBy(messages, m => m.created_at)
+    const messages = sortBy(this.props.messages || [], 'created_at')
 
     return <div className={cx('messages-section', {empty: isEmpty(messages)})}
-     ref='messageList'
-     onScroll={onScroll}>
-      {messages.map(m => <Message message={m} key={m.id}/>)}
+      ref={list => this.list = list}
+      onScroll={e => this._handleScroll(e.target)}>
+      {this.props.pending && <div className='message'>Loading...</div>}
+      {messages.map(m =>
+        <Message ref={node => this['message' + m.id] = node} message={m} key={m.id}/>)}
     </div>
   }
 }

8928508b0bc52dcd5fec4332734cdd1d05fb1535
Lawrence Wang <l@lw.io>
2016-11-23 18:52:11 -0800

    cleanup

--- src/util/threads.js
+++ src/util/threads.js
@@ -1,10 +1,11 @@
-import { filter } from 'lodash'
+import { countBy } from 'lodash'
 
-export const unseenThreadCount = (threads, last_viewed_messages_at) => 
-                                   filter(threads, t => threadIsUnnoticed(t, last_viewed_messages_at) && threadIsUnread(t)).length 
+export const unseenThreadCount = (threads, last_viewed_messages_at) =>
+  countBy(threads, t =>
+    threadIsUnnoticed(t, last_viewed_messages_at) && threadIsUnread(t))
 
 export const threadIsUnnoticed = (thread, last_viewed_messages_at) =>
-                                   new Date(last_viewed_messages_at) < new Date(thread.updated_at)
+  new Date(last_viewed_messages_at) < new Date(thread.updated_at)
 
 export const threadIsUnread = thread =>
-                                new Date(thread.last_read_at) < new Date(thread.updated_at)
+  new Date(thread.last_read_at) < new Date(thread.updated_at)

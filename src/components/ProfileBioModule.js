14260d4265ff4da03e19d1062f1b644bb86528b5
Loren Johnson <loren@hellovenado.com>
2016-12-29 00:01:06 -0800

    Clean-up Bio feed module

--- src/components/ProfileBioModule.js
+++ src/components/ProfileBioModule.js
@@ -1,19 +1,60 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-const { func } = React.PropTypes
+import { debounce } from 'lodash'
+import { trackEvent, ADDED_BIO } from '../util/analytics'
 import { updateUserSettings } from '../actions'
-import ListItemTagInput from './ListItemTagInput'
 
-function ProfileBioModule ({ person, dispatch }) {
-  const update = ((path, value) => dispatch(updateUserSettings({[path]: value})))
-  const save = () => {}
-  return <div className='feed-module profile-bio full-column'>
-    <h2>Welcome {person.name}, help everyone get to know you a bit!</h2>
-    <textarea className='form-control short'
-      placeholder='How would you describe yourself in 140 characters?'>
-    </textarea>
-    <button type='button' className='btn-primary' onClick={save}>Save</button>
-  </div>
+class ProfileBioModule extends React.Component {
+  static propTypes = {
+    person: PropTypes.object,
+    dispatch: PropTypes.func
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      length: 0,
+      maxLength: 140,
+      maxLengthReached: false
+    }
+  }
+
+  onTyping = ({ target }) => {
+    const { maxLength } = this.state
+    const text = target.value
+    const { length } = text
+    this.setState({
+      text,
+      length,
+      maxLengthReached: (length > maxLength)
+    })
+  }
+
+  save = () => {
+    const { text } = this.state
+    const { dispatch } = this.props
+    trackEvent(ADDED_BIO, {context: 'onboarding'})
+    return dispatch(updateUserSettings({bio: text}))
+  }
+
+  render () {
+    const { onTyping, save } = this
+    const { maxLengthReached, length } = this.state
+    const { person } = this.props
+    return <div className='feed-module profile-bio full-column'>
+      <h2>Welcome {person.name}, help everyone get to know you a bit!</h2>
+      <textarea className='form-control short' onChange={onTyping}
+        placeholder='How would you describe yourself in 140 characters?'>
+      </textarea>
+      <button type='button' className='btn-primary'
+        disabled={maxLengthReached} onClick={save}>
+        Save
+      </button>
+      <div className={'text-length ' + (maxLengthReached ? 'over-max-length' : '')}>
+        {length} / 140
+      </div>
+    </div>
+  }
 }
 
 export default connect()(ProfileBioModule)

f6500f2119ee3dbcf88c5d16b7da25c4403ddf6f
Lawrence Wang <l@lw.io>
2017-01-03 11:40:52 -0800

    tweak spacing

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import cx from 'classnames'
 import Avatar from './Avatar'
-import { humanDate, present } from '../util/text'
+import { humanDate } from '../util/text'
 import { sanitize } from 'hylo-utils/text'
 var { func, object, bool } = React.PropTypes
 
@@ -20,18 +20,18 @@ class Message extends React.Component {
     const { message, isHeader } = this.props
 
     const person = message.user
-    let text = present(sanitize(message.text)).replace(/\n/g, '<br/>')
+    let text = sanitize(message.text).replace(/\n/g, '<br/>')
 
     return <div className={cx('message', {messageHeader: isHeader})}
-                data-message-id={message.id}>
-      {isHeader && <Avatar person={person}/>}
+      data-message-id={message.id}>
+      {isHeader && <Avatar person={person} />}
       <div className='content'>
         {isHeader && <div>
           <strong className='name'>{sanitize(person.name)}</strong>
           <span className='date'>{humanDate(message.created_at)}</span>
         </div>}
         <div className='text'>
-          <span dangerouslySetInnerHTML={{__html: text}}/>
+          <span dangerouslySetInnerHTML={{__html: text}} />
         </div>
       </div>
     </div>

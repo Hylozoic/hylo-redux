d3965ef847126e835cab3e289f8c9b8f22d83323
Robbie Carlton <robbie.carlton@gmail.com>
2017-02-01 13:11:40 -0700

    add test for ImageModal

--- src/containers/ImageModal.js
+++ src/containers/ImageModal.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Modal } from '../components/Modal'
 import Icon from '../components/Icon'
-const { func, string, bool } = React.PropTypes
+const { func, string } = React.PropTypes
 
-export const leftOffset = () => {
-  if (typeof window === 'undefined') return {}
+const leftOffset = () => {
+  if (typeof window === 'undefined') return 0
   const main = document.getElementById('main')
-  if (!main) return {} // this should be the case only during tests
+  if (!main) return 0 // this should be the case only during tests
   return main.offsetLeft
 }
 
@@ -14,10 +14,6 @@ export default class ImageModal extends React.Component {
   static propTypes = {
     url: string,
     onCancel: func
-  }
-
-  static contextTypes = {
-    isMobile: bool
   }
 
   render () {
@@ -32,8 +28,7 @@ export default class ImageModal extends React.Component {
 
     return <Modal className='modal' id='image-modal' title='' style={modalStyle} onCancel={onCancel}>
       <div className='image-wrapper'>
-        <img src={url} style={imgStyle}>
-        </img>
+        <img src={url} style={imgStyle} />
         <a className='close' onClick={onCancel}>
           <Icon name='Fail' />
         </a>

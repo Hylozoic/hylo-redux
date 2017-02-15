5d7eca97eac5fe0149bda37d3d0d2b4c32290249
Lawrence Wang <l@lw.io>
2017-02-14 17:11:25 -0800

    fix test

--- test/client/components/ImageModal.test.js
+++ test/client/components/ImageModal.test.js
@@ -1,9 +1,17 @@
 require('../support')
 import React from 'react'
 import { shallow } from 'enzyme'
-import ImageModal from '../../src/containers/ImageModal.js'
+import ImageModal from '../../../src/containers/ImageModal.js'
 
 describe('ImageModal', () => {
+  before(() => {
+    document.documentElement.clientWidth = 0
+  })
+
+  after(() => {
+    document.documentElement.clientWidth = undefined
+  })
+
   it('renders correctly', () => {
     const url = 'http://image.com/image.png'
     const node = shallow(<ImageModal url={url} />)

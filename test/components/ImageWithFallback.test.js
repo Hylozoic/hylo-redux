8c49e61d445b5c8707d179ede7664c7e76bac29f
Connor Turland <connorturland@gmail.com>
2017-02-15 16:39:11 -0500

    add test for new component

---
+++ test/components/ImageWithFallback.test.js
@@ -0,0 +1,32 @@
+import '../support'
+import React from 'react'
+import { shallow } from 'enzyme'
+import ImageWithFallback from '../../src/components/ImageWithFallback'
+
+function renderComponent (props) {
+  return shallow(<ImageWithFallback {...props} />)
+}
+
+describe('<ImageWithFallback />', () => {
+
+  it('will initially display the fallback image', () => {
+    const props = {useClass: 'testClass', preferredSrc: 'src1', fallbackSrc: 'src2'}
+    const wrapper = renderComponent(props)
+    expect(wrapper.find('img[src="src2"]').length).to.equal(1)
+    expect(wrapper.find('.testClass').length).to.equal(1)
+  })
+
+  it('will initially insert, but hide the preferred image', () => {
+    const props = {preferredSrc: 'src1', fallbackSrc: 'src2'}
+    const wrapper = renderComponent(props)
+    expect(wrapper.find('img[src="src1"]').length).to.equal(1)
+  })
+
+  it('will display the preferred image once it has loaded', () => {
+    const props = {preferredSrc: 'src1', fallbackSrc: 'src2'}
+    const wrapper = renderComponent(props)
+    wrapper.instance().handleImageLoaded()
+    expect(wrapper.find('img[src="src1"]').length).to.equal(1)
+    expect(wrapper.find('img[src="src2"]').length).to.equal(0)
+  })
+})

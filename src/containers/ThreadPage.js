c99223318979a9d2596c6f9759953ff3395a1bbc
Lawrence Wang <l@lw.io>
2017-02-15 18:03:55 -0800

    load more messages initially
    avoids the “can’t paginate because i can’t scroll to the top” problem

--- src/containers/ThreadPage.js
+++ src/containers/ThreadPage.js
@@ -1,44 +1,36 @@
 import React from 'react'
 import { prefetch } from 'react-fetcher'
+import { compose } from 'redux'
 import { connect } from 'react-redux'
-import {
-  fetchPost, fetchComments, findError
-} from '../actions'
+import { fetchPost, fetchComments, findError } from '../actions'
 import { FETCH_POST } from '../actions/constants'
 import AccessErrorMessage from '../components/AccessErrorMessage'
 import Thread from '../components/Thread'
-import { denormalizedPost, getComments, getPost } from '../models/post'
-const { array, bool, func, object } = React.PropTypes
+import { denormalizedPost, getPost } from '../models/post'
+const { object } = React.PropTypes
 
-@prefetch(({ dispatch, params: { id } }) =>
-  dispatch(fetchPost(id, {minimal: true}))
-  .then(() => dispatch(fetchComments(id, {refresh: true, newest: true, limit: 20}))))
-@connect((state, { params: { id } }) => {
+function ThreadPage ({ post, error }) {
+  if (error) return <AccessErrorMessage error={error} />
+  if (!post || !post.user) return <div className='loading'>Loading...</div>
+  return <Thread post={post} />
+}
+ThreadPage.propTypes = {post: object, error: object}
+
+function fetchToState ({ dispatch, params: { id } }) {
+  return dispatch(fetchPost(id, {minimal: true}))
+  .then(() =>
+    dispatch(fetchComments(id, {refresh: true, newest: true, limit: 50})))
+}
+
+function mapStateToProps (state, { params: { id } }) {
   const post = getPost(id, state)
   return {
     post: post ? denormalizedPost(post, state) : null,
-    comments: getComments(post, state),
     error: findError(state.errors, FETCH_POST, 'posts', id)
   }
-})
-export default class ThreadPage extends React.Component {
-  static propTypes = {
-    post: object,
-    comments: array,
-    error: object,
-    dispatch: func
-  }
-
-  static contextTypes = {
-    isMobile: bool,
-    currentUser: object
-  }
-
-  render () {
-    const { post, error } = this.props
-    if (error) return <AccessErrorMessage error={error} />
-    if (!post || !post.user) return <div className='loading'>Loading...</div>
-
-    return <Thread post={post} />
-  }
 }
+
+export default compose(
+  prefetch(fetchToState),
+  connect(mapStateToProps)
+)(ThreadPage)

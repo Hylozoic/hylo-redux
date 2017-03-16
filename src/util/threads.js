d18a5ce867f59a254edee3cdcdca3dc0c50fdd9f
Connor Turland <connorturland@gmail.com>
2017-03-16 12:54:57 -0400

    edge case where there's no comments

--- src/util/threads.js
+++ src/util/threads.js
@@ -2,7 +2,7 @@ import { countBy, some } from 'lodash'
 
 export const unseenThreadCount = (threads, last_viewed_messages_at) => {
   const unread = t =>
-    threadIsUnnoticed(t, last_viewed_messages_at) && threadIsUnread(t)
+    t.comments && threadIsUnnoticed(t, last_viewed_messages_at) && threadIsUnread(t)
   return threads.filter(unread).length
 }
 
